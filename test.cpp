75d45bb744db0d2cf2d9800e1c7f45ec07ad650f
Ivan <i.stahanov2000@gmail.com>
2026-08-13 13:35:27 +0200

    test

--- test.cpp
+++ test.cpp
@@ -3,5 +3,11 @@
 using namespace std;
 
 int main() {
-    cout << "Hi)";
+    // Создаём окно 800x600 пикселей
+    sf::RenderWindow window(sf::VideoMode({800,600}), "sfml lesson");
+    
+
+    
+
+    return 0;
 }
