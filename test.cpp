9fdf82d73398c031c61f7865ac04f944f2f751ee
Ivan <i.stahanov2000@gmail.com>
2026-08-13 13:41:05 +0200

    test_2_day63

--- test.cpp
+++ test.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 int main() {
     // Создаём окно 800x600 пикселей
-    sf::RenderWindow window(sf::VideoMode({800,600}), "sfml lesson");
-    
+    sf::RenderWindow window(sf::VideoMode({800,600}), "sfml_lesson");
+    while (window.isOpen()) {
+        while(const optional event = window.pollEvent()) {
+            // Обрабатываем события (закрытие окна, нажатия клавиш и т.д.)
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+        // Очищаем экран (заливаем цветом)
+        window.clear(sf::Color::Black);
+        // Показываем то, что нарисовали
+        window.display();
+    }
 
     
 
