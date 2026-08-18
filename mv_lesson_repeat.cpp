43adce2192f451dff3cb27e0872c69b71fe966a2
Ivan <i.stahanov2000@gmail.com>
2026-08-18 15:46:16 +0200

    day_67

---
+++ mv_lesson_repeat.cpp
@@ -0,0 +1,36 @@
+#include <SFML/Graphics.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({800,600}), "mv_lesson_repeat");
+
+    sf::RectangleShape player({20.f, 20.f});
+    player.setFillColor(sf::Color::White);
+    player.setPosition({375.f, 275.f});
+    // !!!
+    sf::Vector2f pos = player.getPosition(); //sf::Vector2f — это структура с двумя полями: x и y (оба float). Обращаться к ним просто через точку:
+    pos.x;
+    pos.y;
+    float speed = 0.1f; // сколько пикселей двигаться за один кадр, скорость
+
+    while (window.isOpen()) {
+        while(const optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            if (player.getPosition().y - speed >= 0.f) {
+                player.move({0.f, -speed});
+            }
+        }
+
+
+        window.clear(sf::Color::Black);
+        window.draw(player);
+        window.display();
+    }
+    // g++ mv_lesson_repeat.cpp -o mv_lesson_repeat.exe -IC:/msys64/ucrt64/include -LC:/msys64/ucrt64/lib -lsfml-graphics -lsfml-window -lsfml-system
+
+}
