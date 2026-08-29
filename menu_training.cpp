f08eb70b617a7162736cc747ce2189778604637c
Ivan <i.stahanov2000@gmail.com>
2026-08-29 21:55:30 +0200

    day77_menu_training

---
+++ menu_training.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+#include <bits/stdc++.h>
+using namespace std;
+
+enum class GameState {
+    Menu,
+    Playing, 
+    GameOver
+}; 
+// enum class — это способ создать свой тип с ограниченным набором именованных значений.
+// Вместо того чтобы гадать, что значит число 0, 1 или 2, у есть понятные имена
+// GameState::Menu куда яснее, чем "переменная равна нулю".
+
+int main() {
+    
+}
