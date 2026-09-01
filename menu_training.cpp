10c090d5e864c9f5d2b8d8d24e0af6abe119a814
Ivan <i.stahanov2000@gmail.com>
2026-09-01 15:30:35 +0200

    second_try

--- menu_training.cpp
+++ menu_training.cpp
@@ -60,4 +60,4 @@ int main() {
     }
 }
 // g++ menu_training.cpp -o menu_training.exe -IC:/msys64/ucrt64/include -LC:/msys64/ucrt64/lib -lsfml-graphics -lsfml-window -lsfml-system
-// will i have a job?
+// where is my commit?
